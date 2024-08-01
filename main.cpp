d0d61ecffde5d61f966d41387944f6c7b10c7a8b
aminnaz93 <Aminnazari040@hotmail.com>
2024-08-01 15:42:06 +0200

    ds

--- main.cpp
+++ main.cpp
@@ -12,9 +12,34 @@ int main(){
 
 ///////////////////////////////////////////////////////
 
-    int x = 30;
+    /*int x = 30;
+    int num;
+
 
     std::cout << x << std::endl;
+
+    std::cout << "Ange en siffra: ";
+    std::cin >> num;
+
+    std::cout << "Siffran är: " << num << std::endl;*/
+
+
+
+    ///////////////////////////////////////////////////////
+
+
+    std::string name;
+
+    std::cout << "Ange ett namn: ";
+    std::getline(std::cin, name);
+
+    std::cout << name << std::endl;
+
+    
+
+    
+   
+
 
 
     return 0;
