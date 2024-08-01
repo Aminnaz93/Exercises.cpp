6893f2511ee74d83d24e2128707e357788ab397f
aminnaz93 <Aminnazari040@hotmail.com>
2024-08-01 15:04:40 +0200

    da

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 
 int main(){
 
+
+    /*
     std::cout << "Hello World!" << std::endl; std::cout << "Baby" << std::endl;
 
-    std::cout << "Ny rad kommer " << std::endl;
-
-    
+    std::cout << "Ny rad kommer " << std::endl;*/
 
 
+///////////////////////////////////////////////////////
+
+    int x = 30;
+
+    std::cout << x << std::endl;
 
 
     return 0;
